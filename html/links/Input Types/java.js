0fca250d399a55aca04b5995c7ec802503e400ed
Diabolcode <110380921+Diabolcode@users.noreply.github.com>
2022-08-01 11:55:01 +0200

    Add files via upload

---
+++ html/links/Input Types/java.js
@@ -0,0 +1,14 @@
+function o1() {
+    document.getElementById('my').style.left = "0px"
+
+}
+
+function c11() {
+    document.getElementById('my').style.left = "-1000px"
+
+
+}
+/*
+oncontextmenu = function() {
+    return false
+}*/
---
+++ html/links/link 1/link.js
@@ -0,0 +1,224 @@
+function mg() {
+    var text = " <!DOCTYPE html > \n <html > \n <head > \n <title> Page Title </title>\n</head> \n <body> \n  <h1> This is a Heading </h1>\n<p>This is a paragraph.</p > \n  </body>\n</html > ";
+    var file_date = new Blob([text], {
+        type: 'text/plain'
+    });
+    var file_test = window.URL.createObjectURL(file_date);
+    var f = document.getElementById('file');
+    f.href = file_test;
+}
+
+function Dark() {
+    var g1 = document.getElementById("A");
+    g1.style.backgroundColor = "#282A35";
+    var g2 = document.getElementById("M");
+    g2.style.backgroundColor = "#282A35"
+    var g3 = document.getElementById("g3");
+
+    g3.style.backgroundColor = "rgb(122, 122, 122)";
+    var g4 = document.getElementById("Q");
+    g4.style.backgroundColor = "rgb(122, 122,122)";
+    var g5 = document.getElementById("b3");
+    g5.style.display = "none";
+    var g6 = document.getElementById("b4");
+    g6.style.display = "inline-block";
+    var g7 = document.getElementById("c");
+    g7.style.color = " #00a2ff";
+    var g8 = document.getElementById("c1");
+    g8.style.color = " #00a2ff";
+    var g9 = document.getElementById("c2");
+    g9.style.color = " #00a2ff";
+    var g10 = document.getElementById("c3");
+    g10.style.color = " #00a2ff";
+    var g11 = document.getElementById("c4");
+    g11.style.color = " #00a2ff";
+    var g12 = document.getElementById("c5");
+    g12.style.color = " #00a2ff";
+    var g13 = document.getElementById("c6");
+    g13.style.color = " #00a2ff";
+    var g14 = document.getElementById("c7");
+    g14.style.color = " #00a2ff";
+    var g15 = document.getElementById("c8");
+    g15.style.color = " #00a2ff";
+    var g16 = document.getElementById("c9");
+    g16.style.color = " #00a2ff";
+    var g17 = document.getElementById("c10");
+    g17.style.color = " #00a2ff";
+    var g18 = document.getElementById("c11");
+    g18.style.color = " #00a2ff";
+    var g19 = document.getElementById("c12");
+    g19.style.color = " #00a2ff";
+    var g20 = document.getElementById("c13");
+    g20.style.color = " #00a2ff";
+    var g21 = document.getElementById("c14");
+    g21.style.color = " #00a2ff";
+    var g22 = document.getElementById("c15");
+    g22.style.color = " #00a2ff";
+    var g23 = document.getElementById("c16");
+    g23.style.color = " #00a2ff";
+    var g24 = document.getElementById("c17");
+    g24.style.color = " #00a2ff";
+    var g25 = document.getElementById("c18");
+    g25.style.color = "#00a2ff";
+    var g26 = document.getElementById("c19");
+    g26.style.color = " #00a2ff";
+    var g27 = document.getElementById("c20");
+    g27.style.color = " #00a2ff";
+    var g28 = document.getElementById("c21");
+    g28.style.color = " #00a2ff";
+    var g29 = document.getElementById("c22");
+    g29.style.color = " #00a2ff";
+    var g30 = document.getElementById("c23");
+    g30.style.color = " #00a2ff";
+    var g31 = document.getElementById("c24");
+    g31.style.color = " #00a2ff";
+    var g32 = document.getElementById("c25");
+    g32.style.color = " #00a2ff";
+    var m = document.getElementById("m");
+    m.style.color = "#ddd"
+    var m1 = document.getElementById("m1");
+    m1.style.color = "#ddd"
+    var m2 = document.getElementById("m2");
+    m2.style.color = "#ddd"
+    var m3 = document.getElementById("m3");
+    m3.style.color = "#ddd"
+    var m4 = document.getElementById("m4");
+    m4.style.color = "#ddd"
+    var m5 = document.getElementById("m5");
+    m5.style.color = "#ddd"
+    var m6 = document.getElementById("m6");
+    m6.style.color = "#ddd"
+    var m7 = document.getElementById("m7");
+    m7.style.color = "#ddd"
+    var m8 = document.getElementById("m8");
+    m8.style.color = "#ddd"
+    var m9 = document.getElementById("m9");
+    m9.style.color = "#ddd"
+    var m10 = document.getElementById("m10");
+    m10.style.color = "#ddd"
+    var m11 = document.getElementById("m11");
+    m11.style.color = "#ddd"
+    var n = document.getElementById("n");
+    n.style.color = "#d400ff"
+    var n1 = document.getElementById("n1");
+    n1.style.color = "#00ff4c"
+
+    var v9 = document.getElementById("v1");
+    v9.style.color = "#ace"
+    var v1 = document.getElementById("v2");
+    v1.style.color = "#ace"
+    var v = document.getElementById("v3");
+    v.style.color = "#ace"
+
+
+
+
+
+
+}
+
+function D() {
+    var g1 = document.getElementById("A");
+    g1.style.backgroundColor = "rgb(255, 255, 255)"
+
+    var g2 = document.getElementById("M");
+    g2.style.backgroundColor = "rgb(255, 255, 255)"
+    var g3 = document.getElementById("g3");
+    g3.style.backgroundColor = "rgb(209, 209, 209)";
+    var g4 = document.getElementById("Q");
+    g4.style.backgroundColor = "rgb(209, 209, 209)";
+    var g5 = document.getElementById("b3");
+    g5.style.display = "inline-block";
+
+    var g6 = document.getElementById("b4");
+    g6.style.display = "none";
+    var g7 = document.getElementById("c");
+    g7.style.color = " #0000cd";
+    var g8 = document.getElementById("c1");
+    g8.style.color = " #0000cd";
+    var g9 = document.getElementById("c2");
+    g9.style.color = " #0000cd";
+    var g10 = document.getElementById("c3");
+    g10.style.color = " #0000cd";
+    var g11 = document.getElementById("c4");
+    g11.style.color = " #0000cd";
+    var g12 = document.getElementById("c5");
+    g12.style.color = " #0000cd";
+    var g13 = document.getElementById("c6");
+    g13.style.color = " #0000cd";
+    var g14 = document.getElementById("c7");
+    g14.style.color = " #0000cd";
+    var g15 = document.getElementById("c8");
+    g15.style.color = " #0000cd";
+    var g16 = document.getElementById("c9");
+    g16.style.color = " #0000cd";
+    var g17 = document.getElementById("c10");
+    g17.style.color = " #0000cd";
+    var g18 = document.getElementById("c11");
+    g18.style.color = " #0000cd";
+    var g19 = document.getElementById("c12");
+    g19.style.color = " #0000cd";
+    var g20 = document.getElementById("c13");
+    g20.style.color = " #0000cd";
+    var g21 = document.getElementById("c14");
+    g21.style.color = " #0000cd";
+    var g22 = document.getElementById("c15");
+    g22.style.color = " #0000cd";
+    var g23 = document.getElementById("c16");
+    g23.style.color = " #0000cd";
+    var g24 = document.getElementById("c17");
+    g24.style.color = " #0000cd";
+    var g25 = document.getElementById("c18");
+    g25.style.color = "#0000cd";
+    var g26 = document.getElementById("c19");
+    g26.style.color = " #0000cd";
+    var g27 = document.getElementById("c20");
+    g27.style.color = " #0000cd";
+    var g28 = document.getElementById("c21");
+    g28.style.color = " #0000cd";
+    var g29 = document.getElementById("c22");
+    g29.style.color = " #0000cd";
+    var g30 = document.getElementById("c23");
+    g30.style.color = " #0000cd";
+    var g31 = document.getElementById("c24");
+    g31.style.color = " #0000cd";
+    var g32 = document.getElementById("c25");
+    g32.style.color = " #0000cd";
+    var m = document.getElementById("m");
+    m.style.color = "#900"
+    var m1 = document.getElementById("m1");
+    m1.style.color = "#900"
+    var m2 = document.getElementById("m2");
+    m2.style.color = "#900"
+    var m3 = document.getElementById("m3");
+    m3.style.color = "#900"
+    var m4 = document.getElementById("m4");
+    m4.style.color = "#900"
+    var m5 = document.getElementById("m5");
+    m5.style.color = "#900"
+    var m6 = document.getElementById("m6");
+    m6.style.color = "#900"
+    var m7 = document.getElementById("m7");
+    m7.style.color = "#900"
+    var m8 = document.getElementById("m8");
+    m8.style.color = "#900"
+    var m9 = document.getElementById("m9");
+    m9.style.color = "#900"
+    var m10 = document.getElementById("m10");
+    m10.style.color = "#900"
+    var m11 = document.getElementById("m11");
+    m11.style.color = "#900";
+    var n = document.getElementById("n");
+    n.style.color = "#900"
+    var n1 = document.getElementById("n1");
+    n1.style.color = "#ff0000"
+  
+
+    var v9 = document.getElementById("v1");
+    v9.style.color = "#000"
+    var v1 = document.getElementById("v2");
+    v1.style.color = "#000"
+    var v = document.getElementById("v3");
+    v.style.color = "#000"
+
+}
